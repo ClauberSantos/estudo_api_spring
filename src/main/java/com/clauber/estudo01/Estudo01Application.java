7458faf3bf9b90067e64a7415aa870179e323b13
Clauber Oliveira Santos <clauber.santos@castgroup.com.br>
2020-12-23 16:14:48 -0300

    Primeiro Commit

---
+++ src/main/java/com/clauber/estudo01/Estudo01Application.java
@@ -0,0 +1,13 @@
+package com.clauber.estudo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Estudo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Estudo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/clauber/estudo01/Estudo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clauber.estudo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Estudo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
